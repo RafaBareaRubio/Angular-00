9e693c67f982fd2d890c1ddc70702e94683d7218
Rafa Barea <rabarub@gmail.com>
2023-10-09 15:12:02 +0200

    r

---
+++ 01-typescript/src/topics/03-functions.ts
@@ -0,0 +1,43 @@
+
+
+// function addNumbers( a: number, b: number) {
+//     return a + b;
+// }
+
+// const otroNumber = ( a: number, b: number) => {
+//     return a + b;
+// }
+// const result = addNumbers(1,3)
+// const result2 = otroNumber(2,3)
+
+// console.log({ result, result2})
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+
+}
+
+const healCharacter = (character: Character, amount: number) => {
+
+
+    character.hp +=amount;
+
+}
+
+const cranex = {
+    name: 'Rafa',
+    hp: 90,
+    showHp() {
+        console.log(`Puntos de vida ${ this.hp }`);
+    }
+}
+
+
+
+
+healCharacter( cranex, 10);
+
+
+export{}
